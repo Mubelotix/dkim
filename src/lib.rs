f08fa8a906091b724e67ab7a4894dacd62b6ba5a
Mubelotix <mubelotix@gmail.com>
2020-06-24 17:01:02 +0200

    readme fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
+//! Work in progress
+//! This release only aims to claim the name because so much features and doc is missing for now. 
+//! Feel free to follow on crates.io to see updates.
+
 pub mod verifier;
 pub mod canonicalization;
 pub mod hash;
 pub mod email;
 pub mod dkim;
-
-/// Work in progress
-/// This release only aims to claim the name because so much features and doc is missing for now. 
-/// Feel free to follow on crates.io to see updates.
 
 pub enum SigningAlgorithm {
     RSASha1,
