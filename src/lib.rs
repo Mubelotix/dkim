07bdb9cef3a60039c84d53199934b2585a6a26af
Mubelotix <mubelotix@gmail.com>
2020-06-25 09:47:49 +0200

    add goals

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
-//! Work in progress
-//! This release only aims to claim the name because so much features and doc is missing for now. 
-//! Feel free to follow on crates.io to see updates.
+//! # Goals
+//! 
+//! When one step is completed, the major part of the crate version is increased.
+//! 
+//! 1. Make it work
+//!     - [x] Relaxed canonicalization algorithm
+//!     - [x] Verifying
+//!     - [ ] Simple canonicalization algorithm
+//!     - [ ] Signing
+//!     - [ ] Documentation
+//! 2. Make it fast
+//!     - [ ] Benchmarks
+//!     - [ ] Compare to other implementations
+//!     - [ ] Optimize
 
 pub mod verifier;
 pub mod canonicalization;
