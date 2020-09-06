e38b4c1d27f5a8faeaa191d069258bf62af7c481
Mubelotix <mubelotix@gmail.com>
2020-09-06 21:33:44 +0200

    Format

--- src/parsing/dkim_quoted_printable.rs
+++ src/parsing/dkim_quoted_printable.rs
@@ -1,4 +1,4 @@
-use super::header::{ParsingError, is_valchar, is_wsp};
+use super::header::{is_valchar, is_wsp, ParsingError};
 
 pub fn dkim_quoted_printable(input: &str) -> nom::IResult<&str, String, ParsingError> {
     let mut chars = input.chars().enumerate();
@@ -7,7 +7,7 @@ pub fn dkim_quoted_printable(input: &str) -> nom::IResult<&str, String, ParsingE
     loop {
         let (idx, character) = match chars.next() {
             Some(character) => character,
-            None => break
+            None => break,
         };
 
         match character {
@@ -15,12 +15,14 @@ pub fn dkim_quoted_printable(input: &str) -> nom::IResult<&str, String, ParsingE
                 last_valid_idx = idx + 1;
                 result.push(character);
             }
-            character if character == '='=> {
+            character if character == '=' => {
                 if let (Some((_, c1)), Some((idx, c2))) = (chars.next(), chars.next()) {
                     let mut number = String::new();
                     number.push(c1);
                     number.push(c2);
-                    let character: char = u8::from_str_radix(&number, 16).map_err(|_e| ParsingError::InvalidDkimQuotedPrintable.into())? as char;
+                    let character: char = u8::from_str_radix(&number, 16)
+                        .map_err(|_e| ParsingError::InvalidDkimQuotedPrintable.into())?
+                        as char;
                     result.push(character);
                     last_valid_idx = idx + 1;
                 } else {
@@ -30,17 +32,17 @@ pub fn dkim_quoted_printable(input: &str) -> nom::IResult<&str, String, ParsingE
             character if character == '\r' => {
                 match chars.next() {
                     Some((_, '\n')) => (),
-                    _ => return Err(ParsingError::ExpectedLineFeed.into())
+                    _ => return Err(ParsingError::ExpectedLineFeed.into()),
                 }
                 match chars.next() {
                     Some((_, character)) if is_wsp(character) => (),
-                    _ => return Err(ParsingError::ExpectedWhitespace.into())
+                    _ => return Err(ParsingError::ExpectedWhitespace.into()),
                 }
-            },
+            }
             character if is_wsp(character) => (),
             _ => {
                 break;
-            },
+            }
         }
     }
 
@@ -53,8 +55,17 @@ mod test {
 
     #[test]
     fn test_dkim_quoted_printable() {
-        assert_eq!(&dkim_quoted_printable("this\r\n is a test").unwrap().1, "thisisatest");
-        assert_eq!(&dkim_quoted_printable("This=20is=20a=20test").unwrap().1, "This is a test");
-        assert_eq!(&dkim_quoted_printable("This=20is=00a=09test").unwrap().1, "This is\u{0}a\ttest");
+        assert_eq!(
+            &dkim_quoted_printable("this\r\n is a test").unwrap().1,
+            "thisisatest"
+        );
+        assert_eq!(
+            &dkim_quoted_printable("This=20is=20a=20test").unwrap().1,
+            "This is a test"
+        );
+        assert_eq!(
+            &dkim_quoted_printable("This=20is=00a=09test").unwrap().1,
+            "This is\u{0}a\ttest"
+        );
     }
 }
