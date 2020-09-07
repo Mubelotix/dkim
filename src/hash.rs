96ff713b05baefb58df2ba56ab5340545ee707f9
Mubelotix <mubelotix@gmail.com>
2020-09-07 21:56:52 +0200

    Fix bug

--- src/hash.rs
+++ src/hash.rs
@@ -6,17 +6,12 @@ pub fn body_hash_sha1(data: &str) -> Vec<u8> {
     hasher.finalize().to_vec()
 }
 
-pub fn data_hash_sha1(headers: &str, dkim_header: (Option<&str>, &str, &str)) -> Vec<u8> {
+pub fn data_hash_sha1(headers: &str, dkim_header: &str) -> Vec<u8> {
     use sha1::{Digest, Sha1};
 
     let mut hasher = Sha1::new();
     hasher.update(headers);
-    if let Some(name) = dkim_header.0 {
-        hasher.update(name);
-        hasher.update(":");
-    }
-    hasher.update(dkim_header.1);
-    hasher.update(dkim_header.2);
+    hasher.update(dkim_header);
     hasher.finalize().to_vec()
 }
 
@@ -28,16 +23,11 @@ pub fn body_hash_sha256(data: &str) -> Vec<u8> {
     hasher.finalize().to_vec()
 }
 
-pub fn data_hash_sha256(headers: &str, dkim_header: (Option<&str>, &str, &str)) -> Vec<u8> {
+pub fn data_hash_sha256(headers: &str, dkim_header: &str) -> Vec<u8> {
     use sha2::{Digest, Sha256};
 
     let mut hasher = Sha256::new();
     hasher.update(headers);
-    if let Some(name) = dkim_header.0 {
-        hasher.update(name);
-        hasher.update(":");
-    }
-    hasher.update(dkim_header.1);
-    hasher.update(dkim_header.2);
+    hasher.update(dkim_header);
     hasher.finalize().to_vec()
 }
